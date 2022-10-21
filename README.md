f1e2898272295d35a4c7e545707863f930ea53ef
hemandev <hemanditwiz@gmail.com>
2022-10-21 23:39:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-jtm3sh
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/hemandev/typescript-jtm3sh)
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+import { render } from '@engagespot/client';
+
+render('#app', {
+  apiKey: 'ENGAGESPOT_API_KEY',
+  userId: 'YOUR_USERS_UNIQUE_ID',
+  theme: {},
+});
